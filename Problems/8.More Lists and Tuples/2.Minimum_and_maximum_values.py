8bb2734f3bc2b8377626da91e0c551bb9170f8ee
Wilpola <71035737+wilpola@users.noreply.github.com>
2021-11-23 13:57:45 +0200

    complete template

--- Problems/8.More Lists and Tuples/2.Minimum_and_maximum_values.py
+++ Problems/8.More Lists and Tuples/2.Minimum_and_maximum_values.py
@@ -1,6 +1,14 @@
 import random
 
-def minimum_and_maximum(integer_list):
+def minimum_maximum(integer_list):
 
     tupl = (_min, _max)
     return tupl
+
+
+l = []
+for i in range(random.randint(15,25)):
+  l.append(random.randint(-150,150))
+           
+print ("List:", l)
+print ("Minimum and maximum:",minimum_maximum(l))
